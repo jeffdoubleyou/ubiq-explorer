6d11c1e32206aa591d9dae68f88f40e74b5a6107
Ubuntu <ubuntu@ip-172-31-6-193.us-west-2.compute.internal>
2017-06-20 22:20:54 +0000

    Add total miners and hashrate to network pools page

--- scripts/controllers/networkPoolController.js
+++ scripts/controllers/networkPoolController.js
@@ -6,6 +6,14 @@ angular.module('Explorer').controller('networkPoolController', function (Network
         });
     }
 
+    $scope.totalHashRate = 0;
+    $scope.totalMiners = 0;
+    $scope.addHashRate = function(hr) {
+        $scope.totalHashRate = $scope.totalHashRate + hr; 
+    }
+    $scope.addMiners = function(cnt) {
+        $scope.totalMiners = $scope.totalMiners + cnt;
+    }
     $scope.init(); 
 
 });
