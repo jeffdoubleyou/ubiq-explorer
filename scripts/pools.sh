4d3c9b2349302b7459354ec60ce1ebd06d458a80
Ubuntu <jeffdoubleyou@gmail.com>
2019-03-15 20:56:41 +0000

    Update backups during import of new block DB

--- scripts/pools.sh
+++ scripts/pools.sh
@@ -9,34 +9,34 @@ go run tools/addPool.go 'Suprnova' 'https://ubiq.suprnova.cc' 'https://ubiq.supr
 go run tools/addPool.go 'AltpOOls' 'http://ubq.altpools.xyz' 'http://ubq.altpools.xyz/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
 go run tools/addPool.go 'Miners Zone' 'http://ubq.miners-zone.net' 'http://ubq.miners-zone.net/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
 go run tools/addPool.go 'hashpower.farm' 'http://ubq.hashpower.farm' 'http://ubq.hashpower.farm/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
-go run tools/addPool.go 'Coinminer.space' 'http://ubiq.coinminer.space/' 'http://ubiq.coinminer.space/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
-go run tools/addPool.go 'HODLPool' 'http://ubiq.hodlpool.com' 'http://ubiq.hodlpool.com/api/stats' 60.000000 13465861583.000000 99.977418 open-ethereum 35427 35419
-go run tools/addPool.go 'Mole-Pool' 'http://ubiq.mole-pool.net:83/' 'http://mole-pool.net:5050/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
-go run tools/addPool.go 'ZET Tech' 'http://ubq.zet-tech.eu' 'https://ubq.zet-tech.eu/api/stats' 1.000000 21015761.000000 99.963305 open-ethereum 35427 35414
-go run tools/addPool.go 'eHashCoins' 'http://ubq.ehashcoins.org' 'http://ubq.ehashcoins.org/api/stats' 0.000000 0.000000 13.150987 open-ethereum 35427 4659
-go run tools/addPool.go 'millio.nz' 'http://ubiq.millio.nz' 'http://ubiq.millio.nz/api/stats' 1.000000 211111111.000000 99.988709 open-ethereum 35427 35423
-go run tools/addPool.go 'ubiqpool.com' 'https://ubiqpool.com' 'https://ubiqpool.com/api/stats' 0.000000 0.000000 99.960482 open-ethereum 35427 35413
-go run tools/addPool.go 'ubiqminers.org' 'http://ubiqminers.org' 'http://ubiqminers.org/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
-go run tools/addPool.go 'Kwik Pool Party' 'https://ubq.kwikpool.party' 'https://ubq.kwikpool.party/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
-go run tools/addPool.go 'AikaPool' 'https://aikapool.com/ubiq/' 'https://aikapool.com/ubiq/index.php?page=api&action=getpoolstatus&api_key=05e32baf55143f2df3c1c8a5f18bfd454da2db212c67f5a48d54eb7abe8273c5' 0.000000 0.000000 84.593671 mpos 35427 29969
-go run tools/addPool.go 'ubiqminer.com' 'http://ubiqminer.com' 'http://ubiqminer.com/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
-go run tools/addPool.go 'pool.ubq.tw' 'http://pool.ubq.tw' 'http://pool.ubq.tw/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'UBIQ.cc' 'http://www.ubiq.cc/minerpool' 'http://www.ubiq.cc/pool/stats' 1.000000 80645161.000000 99.997177 open-ethereum 35426 35425
-go run tools/addPool.go 'PoolTo.Be' 'https://ubq.poolto.be' 'https://ubq.poolto.be/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'UBQ.MSKpool.ru' 'http://ubq.mskpool.ru' 'http://ubq.mskpool.ru/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'UBIQPool' 'http://ubiqpool.io' 'http://ubiqpool.io/api/stats' 220.000000 57697669428.000000 99.991532 open-ethereum 35426 35423
-go run tools/addPool.go 'UBIQMine.ca' 'http://ubiqmine.ca' 'http://ubiqmine.ca/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'clona.ru' 'http://clona.ru:3015' 'http://clona.ru:3012/api/stats' 1.000000 146666666.000000 98.286569 open-ethereum 35426 34819
-go run tools/addPool.go 'UBQKings.com' 'https://www.ubqkings.com' 'https://api.ubqkings.com/api/stats' 0.000000 0.000000 0.000000 kings 35426 0
-go run tools/addPool.go 'servehttp.com' 'http://ubiqpool.servehttp.com' 'http://ubiqpool.servehttp.com/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'CoinMiners' 'https://ubiq.coin-miners.info' 'https://ubiq.coin-miners.info/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'PoolCoin.biz' 'http://ubq.poolcoin.biz' 'http://ubq.poolcoin.biz/apiubq/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'Epic Pool' 'http://ubiq.epicpool.club' 'http://ubiq.epicpool.club/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35426 0
-go run tools/addPool.go 'UBQ.ALTpool.pro' 'http://ubq.altpool.pro' 'http://ubq.altpool.pro/api/stats' 0.000000 0.000000 99.858861 open-ethereum 35426 35376
-go run tools/addPool.go 'Whalesburg' 'https://whalesburg.com' 'https://etc.pool.whalesburg.com/api/stats' 0.000000 0.000000 99.949190 open-ethereum 35426 35408
-go run tools/addPool.go 'Anorak.Tech' 'https://ubiq.anorak.tech' 'https://ubiq.anorak.tech/api/stats' 1.000000 71910112.000000 93.383391 open-ethereum 35426 33082
-go run tools/addPool.go 'minerall.io' 'https://minerall.io/' 'https://minerall.io/api/v1/poolstats/ubq' 0.000000 0.000000 0.000000 minerall 35426 0
-go run tools/addPool.go 'MaxHash' 'https://ubiqpool.maxhash.org' 'https://ubiqpool.maxhash.org/api/stats' 183.000000 41237894199.000000 99.952013 open-ethereum 35426 35409
-go run tools/addPool.go 'DigiPools.org' 'https://ubiq.digipools.org' 'https://ubiq.digipools.org/api/stats' 0.000000 0.000000 99.870152 open-ethereum 35426 35380
-go run tools/addPool.go 'BaikalMine' 'http://pool.baikalmine.com/ru/ubiq/' 'http://ubiq.baikalmine.com:1050/api/stats' 0.000000 0.000000 99.947631 open-ethereum 24824 24811
-go run tools/addPool.go 'Cryptodig.Ru' 'http://ubiq-solo.cryptodig.ru' 'http://ubiq-solo.cryptodig.ru/api/stats' 0.000000 0.000000 98.920281 open-ethereum 22228 21988
+go run tools/addPool.go 'Coinminer.space' 'http://ubiq.coinminer.space/' 'http://ubiq.coinminer.space/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
+go run tools/addPool.go 'HODLPool' 'http://ubiq.hodlpool.com' 'http://ubiq.hodlpool.com/api/stats' 60.000000 13341135362.000000 99.977419 open-ethereum 35428 35420
+go run tools/addPool.go 'Mole-Pool' 'http://ubiq.mole-pool.net:83/' 'http://mole-pool.net:5050/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
+go run tools/addPool.go 'ZET Tech' 'http://ubq.zet-tech.eu' 'https://ubq.zet-tech.eu/api/stats' 1.000000 15864022.000000 99.963306 open-ethereum 35428 35415
+go run tools/addPool.go 'eHashCoins' 'http://ubq.ehashcoins.org' 'http://ubq.ehashcoins.org/api/stats' 0.000000 0.000000 13.150615 open-ethereum 35428 4659
+go run tools/addPool.go 'millio.nz' 'http://ubiq.millio.nz' 'http://ubiq.millio.nz/api/stats' 1.000000 193693693.000000 99.988709 open-ethereum 35428 35424
+go run tools/addPool.go 'ubiqpool.com' 'https://ubiqpool.com' 'https://ubiqpool.com/api/stats' 0.000000 0.000000 99.960483 open-ethereum 35428 35414
+go run tools/addPool.go 'ubiqminers.org' 'http://ubiqminers.org' 'http://ubiqminers.org/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
+go run tools/addPool.go 'Kwik Pool Party' 'https://ubq.kwikpool.party' 'https://ubq.kwikpool.party/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
+go run tools/addPool.go 'AikaPool' 'https://aikapool.com/ubiq/' 'https://aikapool.com/ubiq/index.php?page=api&action=getpoolstatus&api_key=05e32baf55143f2df3c1c8a5f18bfd454da2db212c67f5a48d54eb7abe8273c5' 0.000000 0.000000 84.591284 mpos 35428 29969
+go run tools/addPool.go 'ubiqminer.com' 'http://ubiqminer.com' 'http://ubiqminer.com/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35428 0
+go run tools/addPool.go 'pool.ubq.tw' 'http://pool.ubq.tw' 'http://pool.ubq.tw/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'UBIQ.cc' 'http://www.ubiq.cc/minerpool' 'http://www.ubiq.cc/pool/stats' 1.000000 94425483.000000 99.997177 open-ethereum 35427 35426
+go run tools/addPool.go 'PoolTo.Be' 'https://ubq.poolto.be' 'https://ubq.poolto.be/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'UBQ.MSKpool.ru' 'http://ubq.mskpool.ru' 'http://ubq.mskpool.ru/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'UBIQPool' 'http://ubiqpool.io' 'http://ubiqpool.io/api/stats' 220.000000 58037251641.000000 99.991532 open-ethereum 35427 35424
+go run tools/addPool.go 'UBIQMine.ca' 'http://ubiqmine.ca' 'http://ubiqmine.ca/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'clona.ru' 'http://clona.ru:3015' 'http://clona.ru:3012/api/stats' 1.000000 160000000.000000 98.286618 open-ethereum 35427 34820
+go run tools/addPool.go 'UBQKings.com' 'https://www.ubqkings.com' 'https://api.ubqkings.com/api/stats' 0.000000 0.000000 0.000000 kings 35427 0
+go run tools/addPool.go 'servehttp.com' 'http://ubiqpool.servehttp.com' 'http://ubiqpool.servehttp.com/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'CoinMiners' 'https://ubiq.coin-miners.info' 'https://ubiq.coin-miners.info/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'PoolCoin.biz' 'http://ubq.poolcoin.biz' 'http://ubq.poolcoin.biz/apiubq/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'Epic Pool' 'http://ubiq.epicpool.club' 'http://ubiq.epicpool.club/api/stats' 0.000000 0.000000 0.000000 open-ethereum 35427 0
+go run tools/addPool.go 'UBQ.ALTpool.pro' 'http://ubq.altpool.pro' 'http://ubq.altpool.pro/api/stats' 0.000000 0.000000 99.858865 open-ethereum 35427 35377
+go run tools/addPool.go 'Whalesburg' 'https://whalesburg.com' 'https://etc.pool.whalesburg.com/api/stats' 0.000000 0.000000 99.949191 open-ethereum 35427 35409
+go run tools/addPool.go 'Anorak.Tech' 'https://ubiq.anorak.tech' 'https://ubiq.anorak.tech/api/stats' 1.000000 69390039.000000 93.383577 open-ethereum 35427 33083
+go run tools/addPool.go 'minerall.io' 'https://minerall.io/' 'https://minerall.io/api/v1/poolstats/ubq' 0.000000 0.000000 0.000000 minerall 35427 0
+go run tools/addPool.go 'MaxHash' 'https://ubiqpool.maxhash.org' 'https://ubiqpool.maxhash.org/api/stats' 184.000000 41009425111.000000 99.952014 open-ethereum 35427 35410
+go run tools/addPool.go 'DigiPools.org' 'https://ubiq.digipools.org' 'https://ubiq.digipools.org/api/stats' 0.000000 0.000000 99.870156 open-ethereum 35427 35381
+go run tools/addPool.go 'BaikalMine' 'http://pool.baikalmine.com/ru/ubiq/' 'http://ubiq.baikalmine.com:1050/api/stats' 0.000000 0.000000 99.947633 open-ethereum 24825 24812
+go run tools/addPool.go 'Cryptodig.Ru' 'http://ubiq-solo.cryptodig.ru' 'http://ubiq-solo.cryptodig.ru/api/stats' 0.000000 0.000000 98.920329 open-ethereum 22229 21989
